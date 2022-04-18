b45a24d53f034cf77fbb6abf5a83888beba7ed42
sujit <sujitpalo149@gmail.com>
2022-04-18 17:13:39 +0530

    rolldex initializing app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+const App=()=>{
+  let a=10
+  let b=20
+  return (
+      
+      <div>
+          <h1>
+               Monster App
+          </h1>
+          <p>Building Crud App</p>
+          <p1>some calculation:{a+b}</p1>
+      </div>
+  )
+}
+
+export default App
